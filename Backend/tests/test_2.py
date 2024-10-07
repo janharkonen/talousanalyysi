f3a235a4cf639638482a2985d8c662b9674cb2f5
Jan Härkönen <jan.harkonen@gmail.com>
2024-10-07 13:00:49 +0300

    initial boilerplate

---
+++ Backend/tests/test_2.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestDataTransformation(unittest.TestCase):
+
+    def test_runko(self):
+        self.assertEqual(0, 0)
+        self.assertEqual(1, 1)
+
+if __name__ == '__main__':
+    unittest.main()
